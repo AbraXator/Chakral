3b5f6e37ba97c8f5d34313fc4929b3e82a87bbfa
AbraXator <adammek.mack@gmail.com>
2022-08-01 23:10:03 +0200

    it works

--- src/main/java/net/AbraXator/chakramod/items/custom/GoldenNecklace.java
+++ src/main/java/net/AbraXator/chakramod/items/custom/GoldenNecklace.java
@@ -8,6 +8,7 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.Style;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
