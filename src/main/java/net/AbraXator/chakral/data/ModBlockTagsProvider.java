81fc78b34be68a7f15c1b7c2feb86396587a3d81
Nikdo53 <usivatel@gmail.com>
2023-04-13 17:25:35 +0200

    Revert "Update ModBlockTagsProvider.java"
    This reverts commit ff3b746fc1a611ea713b4cdfad2084fcd147f1f7.

--- src/main/java/net/AbraXator/chakral/data/ModBlockTagsProvider.java
+++ src/main/java/net/AbraXator/chakral/data/ModBlockTagsProvider.java
@@ -32,12 +32,8 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
         });
         ModBlocks.BLOCKS.getEntries().forEach(blockRegistryObject -> {
             Block block = blockRegistryObject.get();
-            List<RegistryObject<Block>> wood = List.of(ModBlocks.WILTED_LOG, ModBlocks.WILTED_PLANKS, ModBlocks.SHARD_REFINER,);
-            List<RegistryObject<Block>> stone_block = List.of(ModBlocks.BLACK_MINERAL_IRON_ORE, ModBlocks.NECKLACE_SLOTTER, ModBlocks.BLACK_MINERAL_COPPER_ORE, ModBlock.BLACK_MINERAL, ModBlock.WHITE_MINERAL, ModBlock.GREEN_MINERAL, ModBlock.RED_MINERAL, ModBlock.ORANGE_MINERAL, ModBlock.BLUE_MINERAL, ModBlock.LIGHT_BLUE_MINERAL, ModBlock.YELLOW_MINERAL);
-            List<RegistryObject<Block>> iron_block = List.of(ModBlocks.BLACK_MINERAL_DIAMOND_ORE, ModBlocks.BLACK_MINERAL_LAPIS_ORE, ModBlocks.BLACK_MINERAL_GOLD_ORE, ModBlocks.BLACK_MINERAL_REDSTONE_ORE, ModBlocks.BLACK_MINERAL_EMERALD_ORE, ModBlocks.MINERAL_ENRICHER);
-            List<RegistryObject<Block>> leaves = List.of(ModBlocks.WILTED_LEAVES);
-            List<RegistryObject<Block>> dirt = List.of(ModBlocks.MINERAL_RICH_DIRT, ModBlocks.MINERAL_RICH_FARMLAND, ModBlocks.BURGEONING_ROOTS, ModBlocks.MINERAL_RICH_GRASS);
-
+            List<RegistryObject<Block>> stone_block = List.of(ModBlocks.BLACK_MINERAL_IRON_ORE, ModBlocks.BLACK_MINERAL_COPPER_ORE);
+            List<RegistryObject<Block>> iron_block = List.of(ModBlocks.BLACK_MINERAL_DIAMOND_ORE, ModBlocks.BLACK_MINERAL_LAPIS_ORE, ModBlocks.BLACK_MINERAL_GOLD_ORE, ModBlocks.BLACK_MINERAL_REDSTONE_ORE, ModBlocks.BLACK_MINERAL_EMERALD_ORE);
 
             if(!(block.asItem().getDefaultInstance().is(ModTags.Items.MINERAL_RICH) || block.asItem().getDefaultInstance().is(ModTags.Items.SHROOMS))) {
                 this.tag(BlockTags.MINEABLE_WITH_PICKAXE).add(block);
@@ -45,12 +41,6 @@ public class ModBlockTagsProvider extends BlockTagsProvider {
                 this.tag(BlockTags.NEEDS_STONE_TOOL).add(block);
             } else if(iron_block.contains(blockRegistryObject)){
                 this.tag(BlockTags.NEEDS_IRON_TOOL).add(block);
-            } else if(wood.contains(blockRegistryObject)){
-                this.tag(BlockTags.MINEABLE_WITH_AXE).add(block);
-            } else if(leaves.contains(blockRegistryObject)){
-                this.tag(BlockTags.MINEABLE_WITH_HOE).add(block);
-            } else if(leaves.contains(blockRegistryObject)){
-                this.tag(BlockTags.MINEABLE_WITH_SHOVEL).add(block);
             }
         });
         this.tag(BlockTags.DIRT).add(ModBlocks.MINERAL_RICH_DIRT.get(), ModBlocks.MINERAL_RICH_GRASS.get());
