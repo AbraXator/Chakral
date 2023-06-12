480c909ac9906343acb6d72b081b87a9f4adcfc6
AbraXator <adammek.mack@gmail.com>
2023-06-12 17:14:51 +0200

    shattered block

--- src/main/java/net/AbraXator/chakral/blocks/ShatteredBlock.java
+++ src/main/java/net/AbraXator/chakral/blocks/ShatteredBlock.java
@@ -4,13 +4,18 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.BlockParticleOption;
 import net.minecraft.core.particles.ParticleTypes;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.EntityCollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class ShatteredBlock extends Block {
     public ShatteredBlock(Properties pProperties) {
@@ -19,12 +24,28 @@ public class ShatteredBlock extends Block {
 
     @Override
     public void fallOn(Level pLevel, BlockState pState, BlockPos pPos, Entity pEntity, float pFallDistance) {
-        if(pEntity instanceof ServerPlayer player && pLevel instanceof ServerLevel level) {
-            pLevel.setBlock(pPos, Blocks.AIR.defaultBlockState(), 3);
+
+    }
+
+    @Override
+    public void entityInside(BlockState pState, Level pLevel, BlockPos pPos, Entity pEntity) {
+        if (pLevel instanceof ServerLevel level && pEntity.fallDistance >= 2.5F) {
+            level.destroyBlock(pPos, false);
             BlockParticleOption blockParticleOption = new BlockParticleOption(ParticleTypes.BLOCK, pState);
             Vec3 vec3 = pPos.getCenter();
+            level.playSound(null, pPos, SoundEvents.DEEPSLATE_BREAK, SoundSource.BLOCKS, 1.0F, 0.1F);
             level.sendParticles(blockParticleOption, vec3.x(), vec3.y(), vec3.z(), 25, 0, 0, 0, 0);
-            super.fallOn(pLevel, pState, pPos, pEntity, pFallDistance);
         }
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState pState, BlockGetter pLevel, BlockPos pPos, CollisionContext pContext) {
+        if(pContext instanceof EntityCollisionContext context){
+            Entity entity = context.getEntity();
+            if (entity != null && entity.fallDistance > 2.5F) {
+                return Shapes.empty();
+            }
+        }
+        return super.getCollisionShape(pState, pLevel, pPos, pContext);
     }
 }
